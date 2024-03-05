4b1434a2950b309e39cac2fbb5207287218cc6b0
victorhugb <victorhugob@hypergroup.com.br>
2024-03-05 08:17:47 -0300

    :sparkles: create header menu and remove default styles

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
+import MenuApp from './components/Header.vue'
 import { RouterView } from 'vue-router'
 </script>
 
 <template>
+  <MenuApp />
   <RouterView />
 </template>
---
+++ src/components/Header.vue
@@ -0,0 +1,55 @@
+<template>
+  <nav class="navbar">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <router-link to="/" class="nav-link">Home</router-link>
+      </li>
+      <li class="nav-item">
+        <router-link to="/" class="nav-link">Clientes</router-link>
+      </li>
+      <li class="nav-item">
+        <router-link to="/about" class="nav-link">Produtos</router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'MenuApp'
+}
+</script>
+
+<style>
+.navbar {
+  background-color: #333;
+  overflow: hidden;
+  display: flex;
+  justify-content: center;
+  padding: 1rem;
+  border-bottom-left-radius: 12px;
+  border-bottom-right-radius: 12px;
+}
+
+.navbar-nav {
+  list-style: none;
+  padding: 0;
+}
+
+.nav-item {
+  float: left;
+}
+
+.nav-link {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 20px;
+  text-decoration: none;
+}
+
+.nav-link:hover {
+  background-color: #ddd;
+  color: black;
+}
+</style>
