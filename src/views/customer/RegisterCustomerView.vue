5302a15be4b8eb72a4aebb591357913f3002a973
victorhugb <victorhugob@hypergroup.com.br>
2024-03-05 09:40:09 -0300

    :recycle: refactor style register customer view

--- src/views/customer/RegisterCustomerView.vue
+++ src/views/customer/RegisterCustomerView.vue
@@ -1,35 +1,37 @@
 <template>
-  <div class="container">
-    <h2>Cadastro de Cliente</h2>
-    <form @submit.prevent="submitForm" class="form">
-      <div class="field">
-        <label for="customerName">Nome</label>
-        <input id="customerName" v-model="customer.name" type="text" required class="input" />
-      </div>
-      <div class="field">
-        <label for="customerDocument">Documento</label>
-        <input
-          id="customerDocument"
-          v-model="customer.document"
-          type="text"
-          required
-          class="input"
-        />
-      </div>
-      <div class="field">
-        <label for="customerEmail">Email</label>
-        <input id="customerEmail" v-model="customer.email" type="email" required class="input" />
-      </div>
-      <div class="field">
-        <label for="customerPhone">Telefone</label>
-        <input id="customerPhone" v-model="customer.phone" type="text" required class="input" />
-      </div>
-      <div class="fieldStatus">
-        <label for="customerActive">Ativo:</label>
-        <input id="customerActive" v-model="customer.active" type="checkbox" />
-      </div>
-      <button class="buttonRegister" type="submit">Cadastrar Cliente</button>
-    </form>
+  <div class="page">
+    <div class="wrapper">
+      <h2>Cadastro de Cliente</h2>
+      <form @submit.prevent="submitForm" class="form">
+        <div class="field">
+          <label for="customerName">Nome</label>
+          <input id="customerName" v-model="customer.name" type="text" required class="input" />
+        </div>
+        <div class="field">
+          <label for="customerDocument">Documento</label>
+          <input
+            id="customerDocument"
+            v-model="customer.document"
+            type="text"
+            required
+            class="input"
+          />
+        </div>
+        <div class="field">
+          <label for="customerEmail">Email</label>
+          <input id="customerEmail" v-model="customer.email" type="email" required class="input" />
+        </div>
+        <div class="field">
+          <label for="customerPhone">Telefone</label>
+          <input id="customerPhone" v-model="customer.phone" type="text" required class="input" />
+        </div>
+        <div class="fieldStatus">
+          <label for="customerActive">Ativo:</label>
+          <input id="customerActive" v-model="customer.active" type="checkbox" />
+        </div>
+        <button class="buttonRegister" type="submit">Cadastrar Cliente</button>
+      </form>
+    </div>
   </div>
 </template>
 
@@ -61,3 +63,21 @@ export default {
   }
 }
 </script>
+
+<style>
+.page {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 60%;
+  border: 1px solid #ccc;
+  border-radius: 10px;
+  padding: 1rem;
+}
+</style>
