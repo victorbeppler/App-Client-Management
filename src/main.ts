1826433c77b2ba919ab924daf49bf70e2eca8041
victorhugb <victorhugob@hypergroup.com.br>
2024-03-05 16:14:25 -0300

    :bug:fixed bug in customer save and close

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,11 @@ import App from './App.vue'
 import router from './router'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faPlus, faTrash } from '@fortawesome/free-solid-svg-icons'
+import { faPlus, faTrash, faPencil } from '@fortawesome/free-solid-svg-icons'
 
 library.add(faPlus)
 library.add(faTrash)
+library.add(faPencil)
 
 const app = createApp(App)
 
